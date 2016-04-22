7f7a5e31a4071e75b9691952d5c466af5aadb7c3
Costar93 <kisame_18@hotmail.com>
2016-04-22 17:29:32 +0200

    Solved if username allready exists

--- Practica2/main_app.py
+++ Practica2/main_app.py
@@ -9,34 +9,39 @@ app = Flask(__name__)
 
 #Functions
 
-def save_data(username,fullname,email,password):
-    #change the conn line to open it in the all devices
-    #conn = sqlite3.connect('/home/pi/Desktop/db/users.db')
+def user_exists(username):
     conn = sqlite3.connect('users.db')
-    #if user_exists()
-    #Comparing that the user doesnt exist
-    cursor = conn.execute("SELECT username from users")
+    cursor = conn.execute("SELECT username from users where username=:Id",
+    {"Id": username})
     data = [row for row in cursor]
     for user in data:
         if user[0] == username:
+            print ("1111111111")
+            conn.close()
+            return True
+
+def save_data(username,fullname,email,password):
+    #Check if user_exists()
+    if user_exists(username):
+        print ("2222222222")
+        return False
+
+    else:
+    #Adding user
+        print ("3333333333")
+        try:
+            conn = sqlite3.connect('users.db')
+            conn.execute("insert into users (username,fullname,email,password) values (?, ?, ?, ?)",
+                     (username,
+                      fullname,
+                      email,
+                      password))
+            conn.commit()
+            conn.close()
+            return True
+        except:
             conn.close()
             return False
-    else:
-
-
-    #Adding user
-    try:
-        conn.execute("insert into users (username,fullname,email,password) values (?, ?, ?, ?)",
-                 (username,
-                  fullname,
-                  email,
-                  password))
-        conn.commit()
-        conn.close()
-        return True
-    except:
-        conn.close()
-        return False
 
 def user_data(username,password):
     #change the conn line to open it in the all devices
@@ -82,12 +87,14 @@ def user_register():
         fullname = request.form.get('fullname')
         email = request.form.get('email')
         password = request.form.get('password')
-        #print(username=="", fullname=="", email=="", password=="")
-        if username == "" and fullname == "" and email == "" and password == "":
+        print(username=="", fullname=="", email=="", password=="")
+        if username != "" and fullname != "" and email != "" and password != "":
             if save_data(username,fullname,email,password):
                 return render_template('register_succesfully.html',username=username,fullname=fullname)
+            else:
+                return render_template('register_error.html')
         else:
-            return "Register Error"
+            return render_template('register_error.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def user_login():
