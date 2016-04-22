ba86d59f73c779939d661ff2279823745a37ea18
Costar93 <kisame_18@hotmail.com>
2016-04-22 18:11:38 +0200

    Final practice with SQLite

--- Practica2/main_app.py
+++ Practica2/main_app.py
@@ -16,19 +16,15 @@ def user_exists(username):
     data = [row for row in cursor]
     for user in data:
         if user[0] == username:
-            print ("1111111111")
             conn.close()
             return True
 
 def save_data(username,fullname,email,password):
-    #Check if user_exists()
+    #Check if user_exists() for register
     if user_exists(username):
-        print ("2222222222")
         return False
-
     else:
     #Adding user
-        print ("3333333333")
         try:
             conn = sqlite3.connect('users.db')
             conn.execute("insert into users (username,fullname,email,password) values (?, ?, ?, ?)",
@@ -44,8 +40,7 @@ def save_data(username,fullname,email,password):
             return False
 
 def user_data(username,password):
-    #change the conn line to open it in the all devices
-    #conn = sqlite3.connect('/home/pi/Desktop/db/users.db')
+    #Checking login data
     conn = sqlite3.connect('users.db')
     cursor = conn.execute("SELECT username, password from users where username=:Id",
     {"Id": username})
@@ -59,8 +54,7 @@ def user_data(username,password):
         return False
 
 def get_data():
-    #change the conn line to open it in the all devices
-    #conn = sqlite3.connect('/home/pi/Desktop/db/users.db')
+    #Load data from database
     conn = sqlite3.connect('users.db')
     cursor = conn.execute("SELECT username,fullname,email from users")
     data = [row for row in cursor]
@@ -71,15 +65,18 @@ def get_data():
 
 @app.route('/')
 def hello():
+    #main page
     return render_template('landing_page.html')
 
 @app.route('/show_users', methods=['GET', 'POST'])
 def hist_data():
+    #Show all data from database
     historical_data = get_data()
     return render_template('show_users_table.html',historical_data=historical_data)
 
 @app.route('/insert_user', methods=['GET', 'POST'])
 def user_register():
+    #register webpage
     if request.method == 'GET':
             return render_template('insert_user.html')
     elif request.method == 'POST':
@@ -87,7 +84,6 @@ def user_register():
         fullname = request.form.get('fullname')
         email = request.form.get('email')
         password = request.form.get('password')
-        print(username=="", fullname=="", email=="", password=="")
         if username != "" and fullname != "" and email != "" and password != "":
             if save_data(username,fullname,email,password):
                 return render_template('register_succesfully.html',username=username,fullname=fullname)
@@ -98,6 +94,7 @@ def user_register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def user_login():
+    #login webpage
     if request.method == 'GET':
             return render_template('login_user.html')
     elif request.method == 'POST':
@@ -106,7 +103,7 @@ def user_login():
         if user_data(username,password):
             return render_template('login_succesfully.html',username=username)
         else:
-            return "Error Login"
+            return render_template('login_error.html')
 
 if __name__ == '__main__':
     app.debug = True
