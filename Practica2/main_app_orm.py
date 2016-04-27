4d518fc91de48972a1a78a2079c9e4148ccbddbb
Costar93 <kisame_18@hotmail.com>
2016-04-27 23:55:24 +0200

    Insert ORM done, doing query

---
+++ Practica2/main_app_orm.py
@@ -0,0 +1,144 @@
+from __future__ import print_function
+import sys
+import sqlite3
+from flask import render_template
+from flask import Flask, redirect, url_for, request
+from datetime import datetime, date
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy_declarative import User, Base
+
+app = Flask(__name__)
+
+#Functions
+
+def db_session(self):
+        '''Generate a database session used to do queries on the db'''
+        print ("A")
+        # Create engine and bind base to it
+        path_to_db = "mydatabase.db"
+        print ("B")
+        engine = create_engine('sqlite:///' + path_to_db)
+        print ("C")
+        Base.metadata.bind = engine
+        print ("D")
+        # Make a new session and return it
+        DBSession = sessionmaker(bind = engine)
+        print ("E")
+        session = DBSession()
+        return session
+
+def user_exists(username):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.execute("SELECT username from users where username=:Id",
+    {"Id": username})
+    data = [row for row in cursor]
+    if len(data)>0:
+        return True
+    return False
+
+def save_data(username,fullname,email,password):
+    #Check if user_exists() for register
+    if user_exists(username):
+        return False
+    else:
+    #Adding user
+        try:
+            # Create engine and bind base to it
+            path_to_db = "mydatabase.db"
+            engine = create_engine('sqlite:///' + path_to_db)
+            Base.metadata.bind = engine
+            # Make a new session
+            DBSession = sessionmaker(bind = engine)
+            session = DBSession()
+            #saving user
+            new_user = User(username=username, fullname=fullname, email=email, password=password)
+            session.add(new_user)
+            session.commit()
+            return True
+        except:
+            return False
+
+def user_data(username,password):
+    #Checking login data
+    conn = sqlite3.connect('users.db')
+    cursor = conn.execute("SELECT username, password from users where username=:Id",
+    {"Id": username})
+    data = cursor.fetchone()
+    if data is None:
+        return False
+    elif password == data[1]:
+        return True
+    else:
+        return False
+
+
+def get_data():
+    # Create engine and bind base to it
+    path_to_db = "mydatabase.db"
+    engine = create_engine('sqlite:///' + path_to_db)
+    Base.metadata.bind = engine
+    # Make a new session
+    DBSession = sessionmaker(bind = engine)
+    session = DBSession()
+    #searching users
+    session.query(User).all()
+    user = session.query(User).first()
+    print (user.fullname)
+
+
+
+    #Load data from database
+    conn = sqlite3.connect('users.db')
+    cursor = conn.execute("SELECT username,fullname,email from users")
+    data = [row for row in cursor]
+    conn.close()
+    return data
+
+#Routes
+
+@app.route('/')
+def hello():
+    #main page
+    return render_template('landing_page.html')
+
+@app.route('/show_users', methods=['GET', 'POST'])
+def hist_data():
+    #Show all data from database
+    historical_data = get_data()
+    return render_template('show_users_table.html',historical_data=historical_data)
+
+@app.route('/insert_user', methods=['GET', 'POST'])
+def user_register():
+    #register webpage
+    if request.method == 'GET':
+        return render_template('insert_user.html')
+    elif request.method == 'POST':
+        username = request.form.get('username')
+        fullname = request.form.get('fullname')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        if username != "" and fullname != "" and email != "" and password != "":
+            if save_data(username,fullname,email,password):
+                return render_template('register_succesfully.html',username=username,fullname=fullname)
+            else:
+                return render_template('register_error.html')
+        else:
+            return render_template('register_error.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def user_login():
+    #login webpage
+    if request.method == 'GET':
+            return render_template('login_user.html')
+    elif request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if user_data(username,password):
+            return render_template('login_succesfully.html',username=username)
+        else:
+            return render_template('login_error.html')
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run("0.0.0.0")
