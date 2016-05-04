48f60a0749a84074694b4c4b4c1c9eb26a618f8f
Costar93 <kisame_18@hotmail.com>
2016-05-04 20:10:11 +0200

    %s deleted

--- web_server/crud.py
+++ web_server/crud.py
@@ -26,163 +26,8 @@ posts = [
   },
 ]
 
-comments = [
-{
-    "postId": 1,
-    "id": 1,
-    "name": "id labore ex et quam laborum",
-    "email": "Eliseo@gardner.biz",
-    "body": "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
-  },
-  {
-    "postId": 1,
-    "id": 2,
-    "name": "quo vero reiciendis velit similique earum",
-    "email": "Jayne_Kuhic@sydney.com",
-    "body": "est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et"
-  },
-  {
-    "postId": 1,
-    "id": 3,
-    "name": "odio adipisci rerum aut animi",
-    "email": "Nikita@garfield.biz",
-    "body": "quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione"
-  },
-  ]
 
-albums = [
-{
-    "userId": 1,
-    "id": 5,
-    "title": "eaque aut omnis a"
-  },
-  {
-    "userId": 1,
-    "id": 6,
-    "title": "natus impedit quibusdam illo est"
-  },
-  {
-    "userId": 1,
-    "id": 7,
-    "title": "quibusdam autem aliquid et et quia"
-  },
-  ]
 
-photos = [
-{
-    "albumId": 1,
-    "id": 1,
-    "title": "accusamus beatae ad facilis cum similique qui sunt",
-    "url": "http://placehold.it/600/92c952",
-    "thumbnailUrl": "http://placehold.it/150/30ac17"
-  },
-  {
-    "albumId": 1,
-    "id": 2,
-    "title": "reprehenderit est deserunt velit ipsam",
-    "url": "http://placehold.it/600/771796",
-    "thumbnailUrl": "http://placehold.it/150/dff9f6"
-  },
-  {
-    "albumId": 1,
-    "id": 3,
-    "title": "officia porro iure quia iusto qui ipsa ut modi",
-    "url": "http://placehold.it/600/24f355",
-    "thumbnailUrl": "http://placehold.it/150/1941e9"
-  },
-  ]
-
-todos = [
-{
-    "userId": 1,
-    "id": 1,
-    "title": "delectus aut autem",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 2,
-    "title": "quis ut nam facilis et officia qui",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 3,
-    "title": "fugiat veniam minus",
-    "completed": false
-  },
-  ]
-users = [
-{
-    "id": 1,
-    "name": "Leanne Graham",
-    "username": "Bret",
-    "email": "Sincere@april.biz",
-    "address": {
-      "street": "Kulas Light",
-      "suite": "Apt. 556",
-      "city": "Gwenborough",
-      "zipcode": "92998-3874",
-      "geo": {
-        "lat": "-37.3159",
-        "lng": "81.1496"
-      }
-    },
-    "phone": "1-770-736-8031 x56442",
-    "website": "hildegard.org",
-    "company": {
-      "name": "Romaguera-Crona",
-      "catchPhrase": "Multi-layered client-server neural-net",
-      "bs": "harness real-time e-markets"
-    }
-  },
-  {
-    "id": 2,
-    "name": "Ervin Howell",
-    "username": "Antonette",
-    "email": "Shanna@melissa.tv",
-    "address": {
-      "street": "Victor Plains",
-      "suite": "Suite 879",
-      "city": "Wisokyburgh",
-      "zipcode": "90566-7771",
-      "geo": {
-        "lat": "-43.9509",
-        "lng": "-34.4618"
-      }
-    },
-    "phone": "010-692-6593 x09125",
-    "website": "anastasia.net",
-    "company": {
-      "name": "Deckow-Crist",
-      "catchPhrase": "Proactive didactic contingency",
-      "bs": "synergize scalable supply-chains"
-    }
-  },
-  {
-    "id": 3,
-    "name": "Clementine Bauch",
-    "username": "Samantha",
-    "email": "Nathan@yesenia.net",
-    "address": {
-      "street": "Douglas Extension",
-      "suite": "Suite 847",
-      "city": "McKenziehaven",
-      "zipcode": "59590-4157",
-      "geo": {
-        "lat": "-68.6102",
-        "lng": "-47.0653"
-      }
-    },
-    "phone": "1-463-123-4447",
-    "website": "ramiro.info",
-    "company": {
-      "name": "Romaguera-Jacobson",
-      "catchPhrase": "Face to face bifurcated interface",
-      "bs": "e-enable strategic applications"
-    }
-  },
-  ]
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
@@ -191,18 +36,18 @@ def not_found(error):
 def bad_request(error):
     return make_response(jsonify({'error': 'Bad request'}), 400)
 
-@app.route('/%s', methods=['GET'] % (a))
+@app.route('/posts', methods=['GET'])
 def get_posts():
     return jsonify({'posts': posts})
 
-@app.route('/%s/<int:post_id>', methods=['GET'] % (a))
+@app.route('/posts/<int:post_id>', methods=['GET'])
 def get_task(post_id):
     post = [post for post in posts if post['id'] == post_id]
     if len(post) == 0:
         abort(404)
     return jsonify({'post': post[0]})
 
-@app.route('/%s', methods=['POST'] % (a))
+@app.route('/posts', methods=['POST'])
 def create_post():
     if not request.json or not 'title' in request.json or not 'userId' in request.json or not 'body' in request.json:
         abort(400)
@@ -215,7 +60,7 @@ def create_post():
     posts.append(post)
     return jsonify({'post': post}), 201
 
-@app.route('/%s/<int:post_id>', methods=['PUT'] % (a))
+@app.route('/posts/<int:post_id>', methods=['PUT'])
 def update_post(post_id):
     post = [post for post in posts if post['id'] == post_id]
     if len(post) == 0:
@@ -227,7 +72,7 @@ def update_post(post_id):
     return jsonify({'post': post[0]})
     </int:post_id>
 
-@app.route('/%s/<int:post_id>', methods=['DELETE'] % (a))
+@app.route('/posts/<int:post_id>', methods=['DELETE'] )
 def delete_post(post_id):
     post = [post for post in posts if post['id'] == post_id]
     if len(post) == 0:
